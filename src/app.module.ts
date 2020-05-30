93596cbb50b5499f5973ef3fdb49d5cc32be3a58
Amélie Barré <amliebarre@gmail.com>
2020-05-30 18:03:49 +0200

    create Type module and import it in the root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PokemonModule } from './pokemons/pokemon.module';
+import { TypeModule } from './types/type.module';
 
 @Module({
-  imports: [PokemonModule],
+  imports: [
+    PokemonModule,
+    TypeModule,
+  ],
 })
 export class AppModule {}
