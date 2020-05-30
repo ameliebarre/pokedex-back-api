6637bec8c7ed972f4d18cefe1a0f3279a62f6bd2
Amélie Barré <amliebarre@gmail.com>
2020-05-30 18:36:09 +0200

    create Attack module and import it in the root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { PokemonModule } from './pokemons/pokemon.module';
 import { TypeModule } from './types/type.module';
 import { GameModule } from './games/game.module';
+import { AttackModule } from './attacks/attack.module';
 
 @Module({
   imports: [
     PokemonModule,
     TypeModule,
     GameModule,
+    AttackModule,
   ],
 })
 export class AppModule {}
