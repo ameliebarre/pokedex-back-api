3b2082079b27309c3d654c2716c86fc82feb9c2f
Amélie Barré <amliebarre@gmail.com>
2020-05-30 21:36:39 +0200

    register Game schema in its current scope

--- src/games/game.module.ts
+++ src/games/game.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
 import { GameController } from "./controllers/game.controller";
 import { GameService } from "./services/game.service";
+import { GameSchema } from "./schemas/game.schema";
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Game', schema: GameSchema }])],
   controllers: [GameController],
   providers: [GameService],
 })
