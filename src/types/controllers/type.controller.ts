c7301b0d48a139f147d2ae480076b1cbbf38f381
Amélie Barré <amliebarre@gmail.com>
2020-05-30 18:00:55 +0200

    create Type controller with empty request methods

---
+++ src/types/controllers/type.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, Put, Delete, Param, Body } from "@nestjs/common";
+import { TypeService } from "../services/type.service";
+import { UpdateTypeDTO } from "../dto/update-type.dto";
+import { CreateTypeDTO } from "../dto/create-type.dto";
+
+@Controller('types')
+export class TypeController {
+  constructor(private typeService: TypeService) {}
+
+  @Post()
+  async create(@Body() createTypeDTO: CreateTypeDTO) {
+    this.typeService.create(createTypeDTO);
+  }
+
+  @Put(':slug')
+  async update(@Param('slug') slug: string, @Body() updateTypeDTO: UpdateTypeDTO) {}
+
+  @Delete(':slug')
+  async delete(@Param('slug') slug: string): Promise<any> {}
+}
