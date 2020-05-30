79d65e759bc5d7ec72aef989279584ff13fabd8a
Amélie Barré <amliebarre@gmail.com>
2020-05-30 18:34:58 +0200

    create Attack controller

---
+++ src/attacks/controllers/attack.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Param, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { AttackService } from '../services/attack.service';
+import { CreateAttackDTO } from '../dto/create-attack.dto';
+import { UpdateAttackDTO } from '../dto/update-attack.dto';
+
+@Controller('attacks')
+export class AttackController {
+
+  constructor(private attackService: AttackService) {}
+
+  @Post()
+  async create(@Body() createAttackDTO: CreateAttackDTO) {
+    this.attackService.create(createAttackDTO);
+  }
+
+  @Put(':slug')
+  async update(@Param('slug') slug: string, @Body() updateAttackDTO: UpdateAttackDTO) {}
+
+  @Delete(':slug')
+  async delete(@Param('slug') slug: string): Promise<any> {}
+}
