100c5363a0b64a702d4ad4dca04d7701bbfdb694
Amélie Barré <amliebarre@gmail.com>
2020-05-30 18:21:47 +0200

    create Game module and import it in the root component

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PokemonModule } from './pokemons/pokemon.module';
 import { TypeModule } from './types/type.module';
+import { GameModule } from './games/game.module';
 
 @Module({
   imports: [
     PokemonModule,
     TypeModule,
+    GameModule,
   ],
 })
 export class AppModule {}
