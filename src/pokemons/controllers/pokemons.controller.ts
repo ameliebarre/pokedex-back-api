cd430720f8a0d7e892163e3b8d8ffdf8a42bb5b2
Amélie Barré <amliebarre@gmail.com>
2020-05-30 16:12:21 +0200

    create Pokemon DTO

--- src/pokemons/controllers/pokemons.controller.ts
+++ src/pokemons/controllers/pokemons.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Param, Get, Post, Put, Delete } from '@nestjs/common';
+import { Controller, Param, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { CreatePokemonDTO } from '../dto/create-pokemon.dto';
+import { UpdatePokemonDTO } from '../dto/update-pokemon.dto';
 
 @Controller('pokemons')
 export class PokemonsController {
@@ -11,10 +13,10 @@ export class PokemonsController {
   async findOne(@Param('slug') slug: string): Promise<any> {}
 
   @Post()
-  async create(): Promise<any> {}
+  async create(@Body() createPokemonDTO: CreatePokemonDTO): Promise<any> {}
 
   @Put(':slug')
-  async update(@Param('slug') slug: string): Promise<any> {}
+  async update(@Param('slug') slug: string, @Body() updatePokemonDTO: UpdatePokemonDTO): Promise<any> {}
 
   @Delete(':slug')
   async delete(@Param('slug') slug: string): Promise<any> {}
