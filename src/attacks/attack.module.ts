40055b5916d5725464152d1b0efd0e9248b77760
Amélie Barré <amliebarre@gmail.com>
2020-05-30 21:40:49 +0200

    register Attack schema in its current scope

--- src/attacks/attack.module.ts
+++ src/attacks/attack.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
 import { AttackController } from "./controllers/attack.controller";
 import { AttackService } from "./services/attack.service";
+import { AttackSchema } from "./schemas/attack.schema";
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Attack', schema: AttackSchema }])],
   controllers: [AttackController],
   providers: [AttackService],
 })
--- src/attacks/services/attack.service.ts
+++ src/attacks/services/attack.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
 import { Attack } from "../interfaces/attack.interface";
 
 @Injectable()
 export class AttackService {
+
+  constructor(@InjectModel('Attack') private catModel: Model<Attack>) {}
+
   /**
    * Create a new attack
    * @param {Attack} attack
